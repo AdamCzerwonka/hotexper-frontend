d15f3f390af76f1d5c8061134d501d3269aba7a7
Adam Czerwonka <adamczerwonka59@gmail.com>
2023-07-04 19:59:53 +0200

    feat: modify user page to display user info

--- src/Pages/UserDetails.tsx
+++ src/Pages/UserDetails.tsx
@@ -1,12 +1,9 @@
 import { useParams } from "@solidjs/router";
-import { Component, createResource, Match, Suspense, Switch } from "solid-js";
+import { Component, createResource, For, Match, Switch } from "solid-js";
+import Nav from "../Components/Nav";
+import User from "../Types/User";
 
 const UserDetailsPage: Component = () => {
-  interface User {
-    id: string;
-    username: string;
-  }
-
   const params = useParams();
   const fetchUserData = async (userId: string) => {
     return (await fetch(`http://localhost:5062/api/account/${userId}`)).json();
@@ -15,16 +12,32 @@ const UserDetailsPage: Component = () => {
   const [userData] = createResource<User, string>(params.id, fetchUserData);
 
   return (
-    <p>
-    <Switch fallback={JSON.stringify(userData())}>
-    <Match when={userData.loading}>
-    ...loading
-    </Match>
-    <Match when={userData.error}>
-    error
-    </Match>
-    </Switch>
-    </p>
+    <>
+      <div class="max-w-screen-2xl mx-auto">
+        <Nav />
+        <p>
+          <Switch
+            fallback={
+              <div>
+                <p>Id: {userData().id}</p>
+                <p>Email: {userData().email}</p>
+                <p>Username: {userData().username}</p>
+                <h3>Roles</h3>
+                <ul>
+                  <For each={userData().roles}>
+                    {(role, i) => <li>{i()}: {role}</li>}
+                  </For>
+                </ul>
+                <a href={`/users/${params.id}/roles`}>Manage roles</a>
+              </div>
+            }
+          >
+            <Match when={userData.loading}>...loading</Match>
+            <Match when={userData.error}>error</Match>
+          </Switch>
+        </p>
+      </div>
+    </>
   );
 };
 
